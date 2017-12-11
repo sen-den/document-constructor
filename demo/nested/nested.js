fc2cf6567847acd4ebb662a0773b0b017d682a10
Denis Senchishen <senden1998@gmail.com>
2017-12-11 08:03:48 +0200

    Checked elements types

--- demo/nested/nested.js
+++ demo/nested/nested.js
@@ -6,10 +6,10 @@ angular.module("demo").controller("NestedListsDemoController", function($scope) 
     $scope.models = {
         selected: null,
         templates: [
-            {type: "container", title: "Блок", id: 1, columns: [[], []]},
-            {type: "text", title: "Текст", id: 2},
-            {type: "input", title: "Ввод", id: 11},
-            {type: "date", title: "Дата", id: 12},
+            {type: "container", title: "Блок", id: 1, columns: [[]]},
+            {type: "item", title: "Текст", id: 2},
+            {type: "item", title: "Ввод", id: 11},
+            {type: "item", title: "Дата", id: 12},
             
         ],
         dropzones: {
